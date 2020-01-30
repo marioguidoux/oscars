121d415f21d301041979c0c5e639f8aa12cec5fe
marioguidoux <guidoux.gonzaga@gmail.com>
2020-01-30 13:58:10 -0300

    Update json.js

--- json.js
+++ json.js
@@ -5,7 +5,7 @@ var json =
         $.ajax({
             'async': false,
             'global': false,
-            'url': 'https://script.google.com/macros/s/AKfycbxE9d2SujhfVFGmtHsezCjdvPR011jPwCl1uiGm2bmwl2fFxBk/exec',
+            'url': 'https://script.google.com/macros/s/AKfycbwfibJpj0A9c55u4gs5H7zHzx6p6M3ID70zu908/exec',
             'dataType': 'json',
             'success': function (data) {
                 json = data;
